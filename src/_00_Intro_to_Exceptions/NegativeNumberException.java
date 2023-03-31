a5f78c64080f5e67cbbb0b2723eee18221e0846a
psach <psach@DESKTOP-2LVDM0U>
2023-03-31 10:50:09 -0700

    fini

---
+++ src/_00_Intro_to_Exceptions/NegativeNumberException.java
@@ -0,0 +1,31 @@
+package _00_Intro_to_Exceptions;
+
+import javax.swing.JOptionPane;
+
+public class NegativeNumberException extends Exception{
+	public static void scaryPopup() {
+		JOptionPane.showMessageDialog(null, "You have triggered a triggered critical error in your computer!");
+	}
+	
+	
+	public static void testPositive(int x) throws Exception {
+        if (x < 0) {
+            
+            throw new Exception();
+        }
+    }
+	
+	
+	 public static void main(String[] args) {
+		 try {
+				testPositive(-3);
+			} catch (Exception e) {
+				// TODO: handle exception
+				scaryPopup();
+				e.printStackTrace();
+				
+			} finally {
+				JOptionPane.showMessageDialog(null, "Your computer is now okay!");
+			}
+	 }
+}
